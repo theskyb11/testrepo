259832f8dbfce10d887b139c7aed1784627642e1
theskyb11 <akashbachhav2003@gmail.com>
2023-09-10 10:53:02 +0530

    second commit

--- hello.java
+++ hello.java
@@ -1,5 +1,7 @@
 public class hello{
     public static void main(String[] args) {
         System.out.println("Hello");
+        System.out.println("Suresh Pal");
+        System.out.println("Mumbai se hai");
     }
 }
